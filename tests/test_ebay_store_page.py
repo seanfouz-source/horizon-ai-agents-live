a74601adc1c5face82e9bb63b55dd4ac31055b10
seanfouz-source <sean.fouz@gmail.com>
2026-05-26 16:36:44 -0500

    Improve eBay store short-link sync

--- tests/test_ebay_store_page.py
+++ tests/test_ebay_store_page.py
@@ -20,3 +20,47 @@ def test_parse_store_page_finds_listing_card():
     assert items[0].price == 44.5
     assert items[0].quantity == 1
     assert items[0].source == "ebay-store-page"
+
+
+def test_parse_store_page_finds_storefront_state_cards():
+    html = """
+    <script>
+    window.__state__ = {
+      "LISTINGS_MODULE": {
+        "_type": "ContainerModule",
+        "containers": [{
+          "_type": "CardContainer",
+          "cards": [{
+            "_type": "StoreFrontItemCard",
+            "listingId": "366436027346",
+            "action": {
+              "_type": "Action",
+              "URL": "https://www.ebay.com/itm/366436027346?hash=item55514a0bd2:g:M6YAAeSw5AhqFgxb"
+            },
+            "title": {
+              "_type": "TextualDisplay",
+              "textSpans": [{"_type": "TextSpan", "text": "Samsung Galaxy Z Fold5"}]
+            },
+            "image": {
+              "_type": "Image",
+              "URL": "https://i.ebayimg.com/images/g/M6YAAeSw5AhqFgxb/s-l300.jpg"
+            },
+            "displayPrice": {
+              "_type": "TextualDisplayValue",
+              "value": {"value": 675, "currency": "USD"},
+              "textSpans": [{"_type": "TextSpan", "text": "$675.00"}]
+            }
+          }]
+        }]
+      }
+    };
+    </script>
+    """
+
+    items = parse_store_page(html, "https://ebay.us/m/GDmaKw")
+
+    assert len(items) == 1
+    assert items[0].sku == "EBAY-366436027346"
+    assert items[0].title == "Samsung Galaxy Z Fold5"
+    assert items[0].price == 675.0
+    assert items[0].image_url == "https://i.ebayimg.com/images/g/M6YAAeSw5AhqFgxb/s-l300.jpg"
